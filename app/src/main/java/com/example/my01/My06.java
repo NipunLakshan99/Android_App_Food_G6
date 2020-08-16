c1d0515a40f968aeb6ebed55df46c2dc260b7417
HESHANI DASSANAYAKE <heshanidassanayake03@gmail.com>
2020-08-17 05:26:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/my01/My06.java
@@ -0,0 +1,14 @@
+package com.example.my01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class My06 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my06);
+    }
+}
